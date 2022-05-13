cc7274ecdcbf3d5e13f8f1035c5cb5f0c7ec6bec
bsh <dnfg12@naver.com>
2022-05-13 16:31:58 +0900

    게시글 본인만 삭제가능 기능 구현

--- src/main/resources/static/js/board.js
+++ src/main/resources/static/js/board.js
@@ -3,13 +3,16 @@ let index = {
 		$("#btn-save").on("click", () => {		//on("이벤트", 행위), function(){}, 대신에 ()=>{}	this를 바인딩하기위해														 
 			this.save();
 		});
+		
+		$("#btn-delete").on("click", () => {		//on("이벤트", 행위), function(){}, 대신에 ()=>{}	this를 바인딩하기위해														 
+			this.deleteById();
+		});
 	}
 	,
 	save: function() {
 		let data = {
 			title: $("#title").val(),
 			content: $("#content").val(),
-			email: $("#email").val()
 		};
  
 
@@ -21,6 +24,21 @@ let index = {
 			dataType: "json"
 		}).done(function(resp) { 
 			alert("글작성이 완료되었습니다.");
+			location.href = "/";
+		}).fail(function(error) {
+			alert(JSON.stringify(error));
+		});
+	},
+	
+	deleteById: function() {
+		var id = $("#id").text();		//삭제를 할땐 val() 값이 아닌 text() 값을 가져와야함
+		
+		$.ajax({
+			type: "DELETE",
+			url: "/api/board/"+id, 
+			dataType: "json"
+		}).done(function(resp) { 
+			alert("삭제가 완료되었습니다.");
 			location.href = "/";
 		}).fail(function(error) {
 			alert(JSON.stringify(error));
