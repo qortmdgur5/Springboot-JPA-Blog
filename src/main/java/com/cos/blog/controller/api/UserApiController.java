a7b08509867eb6dd67db18569369791bac7608e7
bsh <dnfg12@naver.com>
2022-05-16 16:31:03 +0900

    회원수정기능 구현

--- src/main/java/com/cos/blog/controller/api/UserApiController.java
+++ src/main/java/com/cos/blog/controller/api/UserApiController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,6 +27,14 @@ public class UserApiController {
 		System.out.println("UserApiController : save 호출됨");
 		userService.회원가입(user);
 		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1); // 자바오브젝트를 JSON으로 변환해서 리턴 (Jackson)
+	}
+	
+	@PutMapping("/user")
+	public ResponseDto<Integer> update(@RequestBody User user) { // json데이터로 받으려면 @requestbody 그냥 데이터로 받으려면 key=value, x-www-form-urlencoded
+		userService.회원수정(user);
+		// 여기서 트랜잭션이 종료되기 대문에 DB에 값은 변경이 됌.
+		// 하지만 세션값은 변경되지 않은 상태이기 때문에 우리가 직접 세션값을 변경해 줘야 함.
+		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
 	}
 
 }
